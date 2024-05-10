b8e41c37b7b6937b0f487cc393a0c5e764626444
ADILRADIDI <adilradidi212@gmail.com>
2024-05-10 11:52:21 +0100

    page panier & add product & and filter(color,taille)

--- search.php
+++ search.php
@@ -13,19 +13,22 @@
             align-items: center;
             gap: 10px;
         }
-        .input-search{
+
+        .input-search {
             outline: none;
             border-radius: 10px;
             width: 50%;
             height: 50px;
             background-color: #EDDCAE;
-            color:#808969 ;
+            color: #808969;
         }
-        .input-search:focus{
+
+        .input-search:focus {
             border: 2px solid #9BA986;
-            
+
         }
-        .submit-search{
+
+        .submit-search {
             border-radius: 10px;
             width: 100px;
             height: 50px;
@@ -35,7 +38,8 @@
             font-weight: bold;
             cursor: pointer;
         }
-        .submit-search:hover{
+
+        .submit-search:hover {
             background-color: #808969;
         }
     </style>
@@ -43,12 +47,33 @@
 </head>
 
 <body>
+    <nav class="nav_links">
+        <div class="container_nav">
+            <!-- logo -->
+            <div class="tt" id="logo--text">
+                <h4>HauteHavenBoutique</h4>
+            </div>
+            <!-- links -->
+            <div class="linkss" id="linkss">
+                <a href="index.php" class="links_bold">Home</a>
+            </div>
+
+            <!-- icon links -->
+            <div class="last_links_icon">
+                <!-- icon favourite product -->
+                <a href="#" id="linkLast"><img src="picture/heart.svg" alt="" /></a>
+                <!-- icon panier -->
+                <a href="#" id="linkLast"><img src="picture/shopping cart.svg" alt="" /></a>
+            </div>
+        </div>
+        </div>
+        <hr class="hr1">
+    </nav>
 
     <form action="search.php" method="get">
         <div class="search1">
             <input class="input-search" type="text" name="q" placeholder="Rechercher un produit...">
-            <input  class="submit-search"  type="submit" value="Rechercher"
-            >
+            <input class="submit-search" type="submit" value="Rechercher">
         </div>
     </form>
 
@@ -147,7 +172,62 @@
         }
     }
     ?>
+    <footer id="ContactUs">
+        <div class="container_footer">
+            <div>
+                <h1>Need Help</h1>
+                <a href="#">
+                    <li>Contact Us</li>
+                </a>
+                <a href="#">
+                    <li>Track Order</li>
+                </a>
+                <a href="#">
+                    <li>Returns & Refunds</li>
+                </a>
+                <a href="#">
+                    <li>Career</li>
+                </a>
+            </div>
+            <div>
+                <h1>Company</h1>
+                <a href="#">
+                    <li>About Us</li>
+                </a>
+                <a href="#">
+                    <li>euphoria Blog</li>
+                </a>
+                <a href="#">
+                    <li>euphoriastan</li>
+                </a>
+                <a href="#">
+                    <li>Collaboration</li>
+                </a>
+            </div>
+            <div>
+                <h1>More Info</h1>
+                <a href="#">
+                    <li>Term and Conditions</li>
+                </a>
+                <a href="#">
+                    <li>Privacy Policy
+                    </li>
+                </a>
+                <a href="#">
+                    <li>Shipping Policy
+                    </li>
+                </a>
+                <a href="#">
+                    <li>Sitemap</li>
+                </a>
+            </div>
+        </div>
+        <!--  -->
 
+        <!--  -->
+        <hr class="hr2">
+        <p id="fin-TEXT">Copyright © 2025 HauteHavenBoutique Folks Pvt Ltd. All rights reserved.</p>
+    </footer>
     <script src="main.js"></script>
 </body>
 
